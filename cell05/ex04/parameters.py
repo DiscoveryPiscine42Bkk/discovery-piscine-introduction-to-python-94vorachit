aae8de32ff229a1c8162ea8c806c63fabe78eaf7
Vorachit Hansakul <vohansak@z1t5c1.42bangkok.com>
2025-05-06 13:46:15 +0700

    ex05

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -4,4 +4,4 @@ import sys
 
 # print("Script name:", sys.argv[0])
 print("Number of parameters:", len(sys.argv) - 1)
-# print("Arguments:", sys.argv)
+print("Arguments:", sys.argv[1:])
--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -3,18 +3,11 @@
 import sys
 
 x = len(sys.argv)
+y = sys.argv[1:]
 
-print(x)
-
-# if len(sys.argv) == 1 :
-#     print("none")
-# else :
-#     print("Number of parameters:", len(sys.argv) - 1)
-#     if sys.argv()
-#     print("Arguments:", chr(sys.argv))
-
-# num = int(input("Enter a number\n"))
-# x = 10 
-
-# for i in range(x):
-#    print(i , "x" , num ,"=", num * i)
+if len(sys.argv) == 1 :
+    print("none")
+else :
+    for char in y:
+        if char.isalpha():
+            print(char, end=" ")
