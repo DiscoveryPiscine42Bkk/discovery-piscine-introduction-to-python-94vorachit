d5e29fa9e760b3cecab0eef71e07287ff4b2eb4d
Vorachit Hansakul <vohansak@z1t5c1.42bangkok.com>
2025-05-06 11:45:52 +0700

    ex03

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -3,8 +3,9 @@
 table_num = 0
 while table_num <= 10:
     i = 0
-    print(f"Table de {table_num}:")
+    print(f"Table de {table_num}:", end=" ")
     while i <= 10:       
         print(f"{table_num * i}", end=" ")
         i += 1
+    print()
     table_num += 1
