6a6851d43777214287bba95c07df073f27b05bae
Vorachit Hansakul <vohansak@z1t5c1.42bangkok.com>
2025-05-06 13:57:57 +0700

    ex07

---
+++ cell05/ex07/downcase_it.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+x = len(sys.argv)
+y = sys.argv[1:]
+
+if len(sys.argv) == 1 :
+    print("none")
+else :
+    for i in y:
+        print(i.lower(), end=" ")
