d167825b507d56f4317665c3724898c08396e12c
Vorachit Hansakul <vohansak@z1t5c1.42bangkok.com>
2025-05-06 16:35:40 +0700

    ex11

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+
+x = len(sys.argv)
+#print("parameters: ",x-1)
+y = sys.argv[1:]
+#print(y)
+z = []
+if x == 1:
+    print("none")
+else :
+    for char in y:
+        if char.isalpha():
+            z.append(char)
+    print("parameters: ",len(z))
+    for i in range (len(z)):
+        print(z[i], ":" , len(z[i]))
