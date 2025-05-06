3f88409206a553fc5c08395acc07b9919d14829e
Vorachit Hansakul <vohansak@z1t5c1.42bangkok.com>
2025-05-06 12:12:56 +0700

    ex03

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+a = [2, 8, 9, 48, 8, 22, -12, 2]
+b = [x + 2 for x in a]
+c = []
+print(a)
+print(b)
+for num in b:
+    if num > 5:
+        c.append(num)
+print(c)
+d = set(c)
+print(d)
