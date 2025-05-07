3b8195dfd93bd5dc358ec3113efb7e9058159d29
Vorachit Hansakul <vohansak@z2t5c2.42bangkok.com>
2025-05-07 12:47:08 +0700

    ex09

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -3,26 +3,8 @@
 import sys
 import re
 
-first = sys.argv[2:]
-second = str(sys.argv[1])
-
-print(second)
-#result = re.findall(second,first)
-#print(result)
-#x = len(sys.argv)
-#y = sys.argv[1:]
-#z = []
-##print(x)
-
-if len(sys.argv) <= 2:
-    print("none")
+if len(sys.argv) > 2:
+    result = re.findall(sys.argv[1],sys.argv[2])
+    print(len(result))
 else :
-    result = re.findall(r"second",first)
-    print(result)
-#    for char in y:
-#        if char.isalpha():
-#            z.append(char)
-#    print(z[::-1])
-#    for i in range (-1, -len(z)-1, -1):
-#        print(z[i],end=" ")
-#        print()
+    print("none")
